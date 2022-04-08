c4835988d103a123ba54c3b33cb4632e8ae9048a
Ingrid Lohmann <ingrid.lohmann@db1.com.br>
2022-04-08 00:50:26 -0300

    Implementa teclado e funções de calculo

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-const button = document.getElementById("button");
+const keyboard = document.querySelector(".keyboardContainer");
+const display = document.querySelector(".displayValue");
 
-const firstBinary = document.getElementById("firstBinary");
-const secondBinary = document.getElementById("secondBinary");
+const keys = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*", "C", "=", "«",
+];
 
-let firstNumber;
-let secondNumber;
-
-const getValues = () => {
-    firstNumber = document.getElementById("firstNumber").value;
-    secondNumber = parseInt(document.getElementById("secondNumber").value);
-    return (firstNumber, secondNumber)
+const clean = () => {
+    display.innerHTML = " ";
 }
 
-const getAndDisplayValues = () => {
-    firstNumber = document.getElementById("firstNumber").value;
-    secondNumber = parseInt(document.getElementById("secondNumber").value);
-
-    firstBinary.innerText = `${firstNumber} em binário é ${parseInt(firstNumber).toString(2)}`;
-    secondBinary.innerText = `${secondNumber} em binário é ${parseInt(secondNumber).toString(2)}`;
+const addNumber = (value) => {
+    if (display.textContent == "Erro!") {
+        clean();
+        display.innerHTML += value;
+    } else {
+        display.innerHTML += value;
+    }
 }
 
-button.addEventListener("click", function (e) {
-    e.preventDefault();
 
-    getAndDisplayValues();
+
+const backspace = () => {
+    if (display.textContent) {
+        if (display.textContent == "Erro!") {
+            clean();
+        } else {
+            let displayed = document.getElementById("display").innerHTML;
+            display.innerHTML = displayed.substring(0, displayed.length - 1);
+        }
+    }
+}
+
+const result = () => {
+    if (display.textContent.match("/0")) {
+        document.getElementById("display").innerHTML = "Erro!"
+    } else {
+        document.getElementById("display").innerHTML = eval(display.innerHTML);
+    }
+}
+
+const handleClick = (key) => {
+    switch (key) {
+        case "C":
+            clean()
+            break;
+        case "«":
+            backspace();
+            break;
+        case "=":
+            result();
+            break;
+        default:
+            addNumber(key);
+            break;
+    }
+}
+
+keys.forEach(key => {
+    const keyboardButton = document.createElement("button");
+    keyboardButton.textContent = key;
+    keyboardButton.setAttribute("id", key);
+    keyboardButton.addEventListener("click", () => handleClick(key));
+    keyboard.append(keyboardButton);
 });
-
-
-
-
 
 
