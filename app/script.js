870211d750646e70d3e41885dc62f0968ecffb98
Ingrid Lohmann <ingrid.lohmann@db1.com.br>
2022-04-22 13:19:23 -0300

    ajustes de pastas

--- app/script.js
+++ app/script.js
@@ -198,7 +198,7 @@ const infoAlert = () => {
     Swal.fire(
         {
             width: 500,
-            imageUrl: './assets/infos.png',
+            imageUrl: '../assets/infos.png',
             backdrop: `rgba(216,191,216,0.3)`
         }
     )
