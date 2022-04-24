a915fe825b6773a731575cf67c23b5e9b97b7285
did <didlohmann@hotmail.com>
2022-04-23 23:08:53 -0300

    ajusta pastas

---
+++ report/script.js
@@ -0,0 +1,9 @@
+const infoAlert = () => {
+    Swal.fire(
+        {
+            width: 500,
+            imageUrl: '../assets/infos.png',
+            backdrop: `rgba(216,191,216,0.3)`
+        }
+    )
+}
